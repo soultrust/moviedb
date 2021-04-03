09575aa2f63affad92e0f1be528838a59a8576ad
Tomo Suzuki <tomo@soultrust.com>
2021-04-03 04:41:23 -0400

    More

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,21 @@
 module Api
   module V1
     class AuthenticationController < ApplicationController
+      def create
+        user = User.new(user_params)
+
+        if user.save
+          'saved'
+        else
+          render json: { error: role.errors.messages }, status: 422
+        end
+
+        private
+
+        def user_params
+          params.require(:user).permit(:username, :password_digest)
+        end
+      end
     end
   end
 end
