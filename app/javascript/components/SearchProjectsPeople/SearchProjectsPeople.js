004c9118577b15351f6a45220343afd2c98f7bf0
Tomo Suzuki <tomo@soultrust.com>
2021-05-06 13:49:22 -0400

    Implemented autocomplete from material design

--- app/javascript/components/SearchProjectsPeople/SearchProjectsPeople.js
+++ app/javascript/components/SearchProjectsPeople/SearchProjectsPeople.js
@@ -1,61 +1,112 @@
-import React, { Component, Fragment } from 'react';
-import axios from 'axios';
-// import { FormControl, InputLabel, MenuItem } from '@material-ui/core';
-import NnAutosuggestGroups from '../Forms/NnAutosuggestGroups';
-import classes from '../PersonEdit/AddProjectsSubForm.css';
+/* eslint-disable no-use-before-define */
+import React from 'react';
+import axios from 'axios'
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
-class AddProjectsSubForm extends Component {
-  constructor() {
-    super()
-    this.state = {
-      value: '',
-      suggestions: [],
-      selectedText: ''
+
+export default function SearchProjectsPeople() {
+  const [open, setOpen] = React.useState(false);
+  const [options, setOptions] = React.useState([]);
+  const [value, setValue] = React.useState(null);
+  const [inputValue, setInputValue] = React.useState('');
+  const [loading, setLoading] = React.useState(open && options.length === 0);
+
+  React.useEffect(() => {
+    let active = true;
+
+    (async () => {
+      if (!inputValue) {
+        setOptions([]);
+        return undefined;
+      }
+      setLoading(true);
+      const combinedReqs = ['/api/v1/projects', '/api/v1/persons'].map(url => {
+        return axios.get(`${url}?keywords=${inputValue}`);
+      });
+      const leanFlattened = [];
+
+      await Promise.all(combinedReqs)
+        .then(resultGroups => {
+          resultGroups.forEach(group => {
+            leanFlattened.push(...group.data.data);
+          });
+          return leanFlattened;
+        })
+        .then((options) => {
+          if (active) {
+            setOptions(options);
+            setLoading(false);
+          }
+        })
+        .catch(resp => console.log(resp))
+    })();
+
+    return () => {
+      active = false;
     };
-  }
+  }, [inputValue]);
 
-  handleProjectSelection = (project) => {
-    console.log('handle project selected: ', project)
-    this.setState({
-      role: { ...this.state.role, project_id: project.id },
-      title: project.title
-    })
-  }
+  React.useEffect(() => {
+    if (!open) {
+      setOptions([]);
+    }
+  }, [open]);
 
-  getSuggestionValue = (suggestion) => {
-    const selectedText = suggestion.attributes.title || suggestion.attributes.full_name;
-    this.setState({ selectedText });
-    return selectedText;
-  }
 
-  handleChange = () => (event, { newValue }) => {
-    this.setState({ selectedText: newValue });
-  }
 
-  createSuggestionLabel = (suggestion) => {
-    return `${suggestion.attributes.title || suggestion.attributes.full_name}`
-  }
-
-  render() {
-    return (
-      <Fragment>
-        <h3>Add Projects</h3>
-        <div className="add-project-subform">
-          <div className="form-section">
-            <NnAutosuggestGroups
-              value={this.state.selectedText}
-              onChange={this.handleChange}
-              label="Search projects and people"
-              onItemSelected={this.handleProjectSelection}
-              urls={['/api/v1/projects', '/api/v1/persons']}
-              getSuggestionValue={this.getSuggestionValue}
-              createSuggestionLabel={this.createSuggestionLabel}
-            />
-          </div>
-        </div>
-      </Fragment>
-    )
-  }
+  return (
+    <Autocomplete
+      className="autocomplete"
+      id="grouped-demo"
+      groupBy={(option) => {
+        if (option.type === 'person') {
+          return 'PEOPLE';
+        }
+        return 'PROJECTS'
+      }}
+      getOptionLabel={(option) => {
+        return option.attributes.title || option.attributes.full_name
+      }}
+      value={value}
+      onChange={(_, newValue) => {
+        // setValue(newValue);
+        console.log(newValue)
+      }}
+      inputValue={inputValue}
+      onInputChange={(_, newInputValue) => {
+        setInputValue(newInputValue);
+      }}
+      open={open && inputValue.length > 0}
+      onOpen={() => {
+        setOpen(true);
+      }}
+      onClose={() => {
+        setOpen(false);
+      }}
+      getOptionSelected={(option, value) => {
+        return option.id === value.id;
+      }}
+      options={options}
+      loading={loading}
+      noOptionsText="No results"
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label="Search Projects and People"
+          variant="outlined"
+          InputProps={{
+            ...params.InputProps,
+            endAdornment: (
+              <React.Fragment>
+                {loading ? <CircularProgress color="inherit" size={20} /> : null}
+                {params.InputProps.endAdornment}
+              </React.Fragment>
+            ),
+          }}
+        />
+      )}
+    />
+  );
 }
-
-export default AddProjectsSubForm;
